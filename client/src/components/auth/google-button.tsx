eab55b560aff8e0494960a3940892ca63f91bbb9
Artyom Kulikovski <kartjem@MacBook-Pro-Artyom.local>
2025-03-20 19:52:47 +0200

    .

--- client/src/components/auth/google-button.tsx
+++ client/src/components/auth/google-button.tsx
@@ -11,7 +11,7 @@ export default function GoogleButton({ redirectTo = "/" }: GoogleButtonProps) {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: 'https://qebpndwpnctiisrjjbzo.supabase.co',
+          redirectTo: 'http://scenehunter.com/auth/callback',
         },
       });
 
