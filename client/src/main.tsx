477444882212cc6965686c9993ad1c7f3cd40995
Artyom Kulikovski <kartjem@MacBook-Pro-Artyom.local>
2025-04-11 18:38:03 +0300

    лого для браузера

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 document.documentElement.classList.add("no-transitions");
-
+//
 const theme = localStorage.getItem("theme-store")
   ? JSON.parse(localStorage.getItem("theme-store")!).state.theme
   : "system";
